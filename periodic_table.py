622ebcfa021af61a7c427526cb01ad887f857bce
jaanhavi-bagi <jaanhavibagi@gmail.com>
2025-12-19 08:40:14 +0530

    periodic_table.py

--- periodic_table.py
+++ periodic_table.py
@@ -1,23 +1,23 @@
 import periodic_table_module as ptm
 
 print("Search using atomic number : 1")
-print("Search using atom name : 2")
+print("Search using atom name     : 2")
 print("Search using atomic symbol : 3")
-print("Search a molecule : 4")
+print("Search a molecule          : 4")
 
-a = int(input("Enter how you want to look up element in periodic table : "))
+try:
+    choice = int(input("Enter your choice : "))
 
-if a == 1:
- ptm.atomic_number()
+    if choice == 1:
+        ptm.atomic_number()
+    elif choice == 2:
+        ptm.element_name()
+    elif choice == 3:
+        ptm.atomic_symbol()
+    elif choice == 4:
+        ptm.molecule()
+    else:
+        print("Invalid choice")
 
-elif a == 2:
- ptm.element_name()
-
-elif a == 3:
- ptm.atomic_symbol()
-
-elif a ==4:
- ptm.molecule()
-
-else:
- print("Invalid")
+except ValueError:
+    print("Please enter a valid number (1-4)")
